dca27ddb1035977754cb1606d383582f5e544d7a
ibrahimerkan <erkanibrahim99@gmail.com>
2021-05-05 23:01:14 +0300

    sıfırdan python

---
+++ VBOpythonprogramlama.py
@@ -0,0 +1,205 @@
+#SAYILAR VE STRINGLERE GIRIS
+
+9
+9.2
+9+9
+9*9
+
+print("HELLO AI ERA")
+'HELLO AI ERA'
+
+type(9)
+type(9.2)
+type("HELLO AI ERA")
+
+
+
+#STRİNGLERE YAKINDAN BAKALIM
+
+
+""
+''
+
+123
+type(123)
+"123"
+type("123")
+
+"a" + "b"
+"a" "b"
+"a" " b"
+"a" + "-b"
+
+"a" - "b"
+
+"a "*3
+"a"/3
+
+
+
+#STRING METODLARI - LEN
+
+gel_yaz = "gelecegi_yazanlar"
+#del mvk
+
+a = 9
+b = 10
+
+a*b
+
+
+len(gel_yaz)
+len("gelecegi_yazanlar")
+
+#del a
+#del b
+
+
+
+#STRİNG METODLARI - upper() & lower()
+
+gel_yaz = "gelecegi_yazanlar"
+
+gel_yaz.upper()
+gel_yaz.lower()
+
+gel_yaz.islower()
+B = gel_yaz.upper()
+
+B.islower()
+B.isupper()
+
+
+
+#STRİNG METODLARI - replace()
+
+gel_yaz  = "geleceği_yazanlar"
+
+gel_yaz.replace("e","a")
+
+gel_yaz.replace("a","i")
+
+
+
+#STRİNG METODLARI - strip()
+
+gel_yaz = " gelecegi_yazanlar "
+gel_yaz.strip()
+
+gel_yaz = "*gelecegi_yazanlar*"
+gel_yaz.strip("*")
+
+gel_yaz = "llgelecegi_yazanlar"
+gel_yaz.strip("l")
+
+
+
+#METODLARA GENEL BAKIS
+
+gel_yaz = "gelecegi_yazanlar"
+
+
+dir(gel_yaz)
+
+gel_yaz.capitalize()
+gel_yaz.title()
+
+
+
+#SUBSTRINGLER
+
+gel_yaz = "gelecegi_yazanlar"
+
+gel_yaz[0]
+
+gel_yaz[20]
+
+gel_yaz[0:3]
+
+gel_yaz[3:7]
+
+
+
+#DEGISKENLER
+
+a = 9
+b = "ali_uzaya_git"
+c = a*6
+
+
+a/c
+a*c
+a*5
+
+type(100)
+type(100.2)
+type(1+4j)
+
+a = 100.2
+
+
+
+#TYPE_DONUSUMLERI
+
+toplama_bir = input()
+toplama_iki = input()
+
+
+type(toplama_bir)
+
+toplama_bir + toplama_iki
+
+
+int(toplama_bir) + int(toplama_iki)
+
+
+int(11.0)
+
+12
+
+float(12)
+
+type(str(12))
+
+
+
+#print()
+
+print("HELLO AI ERA")
+
+print("gelecegi","yazanlar")
+
+print("gelecegi","yazanlar", sep = "_")
+
+print()
+
+type()
+
+print()
+
+?print
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
