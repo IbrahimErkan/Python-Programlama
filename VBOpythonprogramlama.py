5293b6bb102f47f22243901dc711bf2d394ca65d
ibrahimerkan <erkanibrahim99@gmail.com>
2021-05-10 16:26:26 +0300

    Update VBOpythonprogramlama.py

--- VBOpythonprogramlama.py
+++ VBOpythonprogramlama.py
@@ -1183,16 +1183,69 @@ impure_sum(6)
 pure_sum(3,7)
 
 
+#Isimsiz Fonksiyonlar (Anonymous Functions)
 
+def old_sum(a,b):
+    return a + b
+
+old_sum(4,5)
+
+ #Lambda
+new_sum = lambda a,b: a + b
+new_sum(4,5)
+
+
+sirasiz_liste = [('b',3),('a',8),('d',12),('c',1)]
+sirasiz_liste
+
+sorted(sirasiz_liste, key = lambda x: x[1])
+
+
+#Vektorel Operasyonlar (Vectorel Operations)
+#OOP
+a = [1,2,3,4]
+b = [2,3,4,5]
+
+ab = []
+
+for i in range(0, len(a)):
+    ab.append(a[i]*b[i])
     
+ab
+
+#FP
+
+import numpy as np
+
+a = np.array([1,2,3,4])
+b = np.array([2,3,4,5])
+
+a*b
 
 
+#map & filter & reduce
+
+liste = [1,2,3,4,5]
+
+for i in liste:
+    print(i+10)
 
 
+list(map(lambda x: x + 10, liste))
+
+#filter 
+
+liste = [1,2,3,4,5,6,7,8,9,10]
+
+list(filter(lambda x: x % 2 == 0, liste))
 
 
+#reduce
 
+from functools import reduce
 
+liste = [1,2,3,4]
+reduce(lambda a,b: a + b, liste)
 
 
 
