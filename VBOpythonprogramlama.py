9bb3a591ebaa40dbd1a3e45cdadb302ce788703e
ibrahimerkan <erkanibrahim99@gmail.com>
2021-05-10 05:22:50 +0300

    Update VBOpythonprogramlama.py

--- VBOpythonprogramlama.py
+++ VBOpythonprogramlama.py
@@ -1064,6 +1064,138 @@ veli = VeriBilimci()
 veli.sql
 veli.bildigi_diller
 
+#Ornek ozellikleri
+
+class VeriBilimci():
+    bildigi_diller = ["R","PYTHON"]
+    bolum = ''
+    sql = ''
+    deneyim_yili = 0
+    def __init__(self):
+        self.bildigi_diller = []
+        self.bolum = ''
+        
+ali = VeriBilimci()
+ali.bildigi_diller
+
+veli = VeriBilimci()
+veli.bildigi_diller
+
+
+ali.bildigi_diller.append("Python")
+ali.bildigi_diller
+
+veli.bildigi_diller
+veli.bildigi_diller.append("R")
+veli.bildigi_diller
+
+VeriBilimci.bildigi_diller
+
+
+VeriBilimci.bolum
+ali.bolum = "istatistik"
+VeriBilimci.bolum
+veli.bolum
+veli.bolum = "end_muh"
+veli.bolum
+ali.bolum
+VeriBilimci.bolum
+
+
+#Ornek Metodlari
+
+class VeriBilimci():
+    calısanlar = []
+    def __init__(self):
+        self.bildigi_diller = []
+        self.bolum = ''
+    def dil_ekle(self, yeni_dil):
+        self.bildigi_diller.append(yeni_dil)
+        
+ali = VeriBilimci()
+ali.bildigi_diller
+ali.bolum
+
+veli = VeriBilimci()
+veli.bildigi_diller
+veli.bolum
+
+dir(VeriBilimci)
+
+VeriBilimci.dil_ekle
+VeriBilimci.dil_ekle("R") # hata verecek
+
+ali.dil_ekle("R")
+ali.bildigi_diller
+
+veli.dil_ekle("Python")
+veli.bildigi_diller
+
+
+
+#Miras Yapilari (inheritance)
+
+class Employees():
+    def __init__(self):
+        self.FirstName = ""
+        self.LastName = ""
+        self.Address = ""
+
+class DataScience(Employees):
+    def __init__(self):
+        self.Programming = ""
+        
+veribilimci1 = DataScience()
+veribilimci1.
+        
+class Marketing(Employees):
+    def __init__(self):
+        self.StoryTelling = ""
+        
+mar1 = Marketing()        
+mar1. 
+
+
+
+#Python'da Fonksiyel Programlama
+
+#Fonksiyonlar dilin bastacidir.
+#Birinci sinif nesnelerdir.
+#Yan etkisiz fonksiyonlardır. (stateless, girdi-cikti)
+#Yuksek seviye fonksiyonlardır.
+#Vektorel operasyonlar.
+
+
+#Yan Etkisiz Fonksiyonlar (Pure Functions)
+
+#Ornek1:Bagimsizlik
+
+A = 9
+
+def impure_sum(b):
+    return b + A
+
+def pure_sum(a,b):
+    return a + b
+
+
+impure_sum(6)
+pure_sum(3,7)
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
 
 
 
